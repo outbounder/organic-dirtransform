ada212f7e57d41df90556bcd0bf80edcd6ff8a13
Boris Filipov <obiwonn@gmail.com>
2013-06-06 12:42:34 +0300

    fixed urlizing [issue on win]

--- plasma/addons/urlize-file.js
+++ plasma/addons/urlize-file.js
@@ -1,5 +1,3 @@
-var path = require("path");
-
 var relativePath = function(file, root) {
   return file.split("\\").join("/").replace(root.split("\\").join("/"), "");
 }
@@ -8,7 +6,7 @@ module.exports = function(file, options, suffixIndexNamed, suffix) {
   var url = relativePath(file, options.targetsRoot).replace("_", ":");
   var isIndexNamed = url.indexOf(options.indexName) !== -1;
   url = url.replace(options.targetExtname, "");
-  url = url.replace(path.sep+options.indexName, "");
+  url = url.replace("/"+options.indexName, "");
   if(options.mount)
     url = options.mount+url;
   if(isIndexNamed)
